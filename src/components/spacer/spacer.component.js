a194bbcdc01930b74b5fd77bb61029ec1aa20242
Divyanshu <divyanshujain599@gmail.com>
2022-06-22 20:54:46 +0530

    mtg part 1 complete

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,22 +1,38 @@
-import styled from "styled-components/native";
+import React from "react";
+import styled, { useTheme } from "styled-components/native";
 
-const sizevariant = {
-    small:1,
-    medium:2,
-    large:3,
-}
-const positionvariant = {
-    top:'marginTop',
-    left:'marginLeft',
-    right:'marginRight',
-    bottom:'marginBottom'
-}
-const getVariant =(position,size) => `${positionvariant[position]}:${sizevariant[size]}`
-export const Spacer = styled.View`
-    ${({position,size})  =>  getVariant(position,size)}
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
+
+const positionVariant = {
+  top: "marginTop",
+  left: "marginLeft",
+  right: "marginRight",
+  bottom: "marginBottom",
+};
+
+const getVariant = (position, size, theme) => {
+  const sizeIndex = sizeVariant[size];
+  const property = positionVariant[position];
+  const value = theme.space[sizeIndex];
+
+  return `${property}:${value}`;
+};
+
+const SpacerView = styled.View`
+  ${({ variant }) => variant};
 `;
 
-Spacer.defaultProps ={
-    position = 'top',
-    size = 'small'
-}
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
+
+Spacer.defaultProps = {
+  position: "top",
+  size: "small",
+};
